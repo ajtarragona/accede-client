cf6b8499472b122f43a24078c963607c89f4b6f5
Txomin Medrano Martorell <tmedrano@tarragona.cat>
2019-02-04 15:11:44 +0100

    readme updated

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
-## ACCEDE Client
+# ACCEDE (Tercers i Vialer) Client for Laravel 5.6
 
-Paquete de acceso al WS de Accede (Aytos)
+## Accede-Tercers
+
+Paquet d'accés al WS de Accede (Aytos) per a Tercers i vialer
+
+
+## Installation
+
+```bash
+composer require ajtarragona/accede-tercers:"@dev"
+```
+
+## Alias
+
+For a simpler use of this package, you may register the alias in the alias array in your `config/app.php` file adding:
+
+```php
+'AccedeTercers' => Ajtarragona\AccedeTercers\Facades\AccedeTercers::class
+```
+
+## Configuration
+
+You should config the module via your .env file. Theese are the available parameters:
+```bash
+ACCEDE_TOKEN_KEY 
+ACCEDE_WS_URL 
+ACCEDE_USER 
+ACCEDE_PASSWORD 
+ACCEDE_CLIENT 
+ACCEDE_ENTITY 
+ACCEDE_ORGANISM 
+```
+
+Also, you may publish the configuration file.
+
+```bash
+php artisan vendor:publish --tag=ajtarragona-accede-tercers
+```
+
+This will copy the configuration file to `config/accede-tercers.php`.
+
+
+
+## Usage
+
+After the configuration, the package will we ready to use. You can use it via Facade:
+
+```bash
+use AccedeTercers;
+...
+public function test(){
+	$vies=AccedeTercers::getAllVies();
+	...
+}
+```
+
+Or via dependency injection in your controllers, helpers o model:
+
+```bash
+use Ajtarragona\AccedeTercers\Models\Accede\AccedeTercersProvider;
+...
+public function test(AccedeTercersProvider $accede){
+	$vies=$accede->getAllVies();
+	...
+}
+```
--- src/Controllers/AccedeTestController.php
+++ src/Controllers/AccedeTestController.php
@@ -4,40 +4,40 @@ namespace Ajtarragona\AccedeTercers\Controllers;
 
 use App\Http\Controllers\Controller;
 use Ajtarragona\AccedeTercers\Models\Accede\AccedeTercersProvider;
-//use Ajtarragona\AccedeTercers\Facades\AccedeTercers; //facade
+use AccedeTercers; //facade
 
 class AccedeTestController extends Controller
 {
 
-	public function test($filter, AccedeTercersProvider $accede){ //con inyeccion
+	// public function test($filter, AccedeTercersProvider $accede){ //con inyeccion
 
-		$tercers=$accede->getTercerByNIF($filter);
-		//dd($tercers);
-
-		$vies=$accede->searchViesByName($filter);
-		if($vies) return $vies;
-		
-		if($tercers){
-			foreach($tercers as $tercer){
-				$domicilios=$accede->getDomicilisTercer($tercer->codigoTercero);
-				$tercer->l_domicilio=$domicilios;
-			}
-			return $tercers;
-		}
-	}
-
-	// public function test($filter){ //con facade
-	
-	// 	$tercers=AccedeTercers::getTercerByNIF($filter);
+	// 	$tercers=$accede->getTercerByNIF($filter);
 	// 	//dd($tercers);
+
+	// 	$vies=$accede->searchViesByName($filter);
+	// 	if($vies) return $vies;
 		
 	// 	if($tercers){
 	// 		foreach($tercers as $tercer){
-	// 			$domicilios=AccedeTercers::getDomicilisTercer($tercer->codigoTercero);
+	// 			$domicilios=$accede->getDomicilisTercer($tercer->codigoTercero);
 	// 			$tercer->l_domicilio=$domicilios;
 	// 		}
 	// 		return $tercers;
 	// 	}
 	// }
+
+	public function test($filter){ //con facade
+	
+		$tercers=AccedeTercers::getTercerByNIF($filter);
+		//dd($tercers);
+		
+		if($tercers){
+			foreach($tercers as $tercer){
+				$domicilios=AccedeTercers::getDomicilisTercer($tercer->codigoTercero);
+				$tercer->l_domicilio=$domicilios;
+			}
+			return $tercers;
+		}
+	}
 }
 
